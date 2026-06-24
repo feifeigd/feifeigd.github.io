7529b1ae0a8f69fe554c41ed855ba938654c3533
feifeigd <feifeigd@21cn.com>
2026-06-24 13:44:10 +0800

    Add SVG illustration for "Focus on What Matters"

---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import type{ ReactNode } from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Heading from '@theme/Heading';
+
+import { clsx } from 'clsx';
+import styles from './index.module.css';
+import Link from '@docusaurus/Link';
+import HomepageFeatures from '../components/HomepageFeatures';
+
+function HomepageHeader(): ReactNode {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx("hero hero--primary", styles.heroBanner)}>
+            <div className="container">
+                <Heading as="h1" className="hero__title">{siteConfig.title}</Heading>
+
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        href="/docs/intro">
+                        进入博客
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default function Home(): ReactNode {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />">
+            
+            <HomepageHeader />
+            <main>
+                <HomepageFeatures />
+            </main>
+        </Layout>
+    );
+}
