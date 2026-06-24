6a89f951ba30b24d1275c498f34ff99f378849fd
feifeigd <feifeigd@21cn.com>
2026-06-24 20:02:01 +0800

    feat: add initial blog posts and tags configuration

--- docs/index.md
+++ docs/index.md
@@ -1,43 +1,5 @@
----
-lang: zh-CN
-title: 首页
-description: 个人博客
-sidebar: auto
-# 禁用导航条
-# navbar: false
 
-# 页面布局 doc home page
-# https://vitepress.dev/reference/default-theme-home-page
-layout: home 
 
-hero:
-    name: d7kj 个人博客
-    text: 个人静态网站
-    tagline: 记录工作和学习过程中的笔记：C/C++，服务器开发，Linux笔记
-    actions:
-    - theme: brand
-      text: Get Started
-      link: /guide/what-is-vitepress
-    - theme: alt
-      text: View on GitHub
-      link: https://github.com/feifeigd
-    - theme: alt
-      text: API Example
-      link: /api-examples
-
-features:
-  - icon: ⚡️
-    title: 个人简介
-    details: 主要涉及技术：C/C++服务器开发，Linux
-  - icon: 🖖
-    title: 开发笔记
-    details: C/C++比较，Linux笔记
-  - icon: 🛠️
-    title: github项目
-    details: 架构设计，设计模式，框架使用
----
-
-<VPTeamMembers size="small" :members="members" />
 
 [首页](./index.md)
  
@@ -47,23 +9,6 @@ features:
 
 # Hello VuePress :tada: ! :100:
 
-
-```ts{1,6-8}
-import { defaultTheme, defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-    title: '你好',
-
-    theme: defaultTheme({
-        logo: 'https://vuejs.org/images/logo.png',
-    })
-})
-```
-
-一加一等于：{{ 1 + 1 }}
-<span v-for="i in 3">span: {{ i }} </span>
-
-![图片](/bg.jpg)
 
 table
 |Tables|Are|Cool|
@@ -92,23 +37,4 @@ This is a dangerous warning
 Danger zone, do not proceed
 :::
 
-<!-- <Page/> -->
 
-<script setup>
-import VPTeamMembers from "vitepress/theme";
-const members = [
-    {
-        avatar: 'https://www.github.com/yyx990803.png',
-        name: 'Evan You',
-        title: 'Creator',
-        links: [
-        { icon: 'github', link: 'https://github.com/yyx990803' },
-        { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
-        ]
-    },
-];
-// config.ts 定义的变量
-const date = __DATE__
-</script>
-
-<pre>Generated: {{ date }} </pre>
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,6 +4,10 @@ const config: Config = {
     title: 'D7kj',
     tagline: '大飞哥的个人博客',
 
+    future: {
+        v4: true,
+    },
+
     url: 'https://feifeigd.github.io',
     // Set the /<baseUrl> pathname under which your site is served.
     // For GitHub pages deployment, it is often '/<projectName>/'
@@ -24,11 +28,56 @@ const config: Config = {
         [
             'classic',
             {
-                docs: false,
-                blog: false,
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    editUrl: 'https://github.com/feifeigd/feifeigd.github.io/edit/main/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: 'all',
+                        xslt: true,
+                    },
+                    editUrl: 'https://github.com/feifeigd/feifeigd.github.io/edit/main/blog/',
+                },
             },
         ],
     ],
+
+    themeConfig: {
+        image: 'img/docusaurus-social-card.jpg',
+        colorMode: {
+            defaultMode: 'light',
+            disableSwitch: false,
+            respectPrefersColorScheme: true,
+        },
+        // 顶部导航条
+        navbar: {
+            title: 'D7kj',
+            logo: {
+                alt: 'D7kj Logo',
+                src: 'img/logo.svg',  // static/img/logo.svg
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Tutorial',
+                },
+                {
+                    to: '/blog',
+                    label: 'Blog',
+                    position: 'left',
+                },
+                {
+                    href: 'https://github.com/feifeigd/feifeigd.github.io',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+    },
 }
 
 export default config;
